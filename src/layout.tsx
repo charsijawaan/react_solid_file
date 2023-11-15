17806124974e4ed5e9065d74b5ba5a00fc11b29c
charsijawaan <shakeelusama3@gmail.com>
2023-11-16 02:29:21 +0500

    added lib for theme

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Lines from "@/components/Lines";
 import Header from "@/components/Header";
-import ToasterContext from "@/context/ToastContext";
+import ToasterContext from "@/lib/ToastContext";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
-import { ThemeProvider } from "next-themes";
 import ScrollToAnchor from "@/components/ScrollToAnchor";
+import { ThemeProvider } from "@/lib/ThemeProvider";
 
 const RootLayout = () => {
   return (
